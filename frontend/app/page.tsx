5a01a95b59b931d53470bc3fac9a162dda6cc954
Will Brammer <williamedwardbrammer@gmail.com>
2024-01-01 08:46:23 -0600

    style: add some style to the landing page navbar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,43 @@
 import Image from 'next/image'
+import "./globals.css";
+import javco_logo from "../public/javco_logo.png"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
-    </main>
+<>
+  <div className="bg-white text-jblue mx-auto max-w-screen-lg flex flex-col lg:flex-row justify-between my-2 shadow-lg shadow-indigo-500/40 rounded px-8 max-w-screen-lg">
+    <div className='flex items-center m-4 lg:order-1'>
+      <Image
+        src={javco_logo}
+        alt='logo'
+        className="h-[37px] w-[140px] min-h-[64px] min-w-[78px] max-h-[140px] max-w-[140px]"
+      />
+    </div>
+    <div className="flex flex-col lg:flex-row lg:order-2">
+      <button
+        className="p-2 px-4 my-4 cursor-pointer font-semibold"
+        // onClick={scrollToSection}
+      >
+        Services
+      </button>
+      <button
+        className="p-2 px-4 my-4 cursor-pointer font-semibold"
+        // onClick={scrollToSection}
+      >
+        About Us
+      </button>
+      <button
+        className="p-2 px-4 my-4 cursor-pointer font-semibold"
+        // onClick={scrollToSection}
+      >
+        Contact Us
+      </button>
+    </div>
+  </div>
+</>
+
+
+
+  
   )
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,8 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        jblue: 'rgb(33, 99, 166)',
     },
   },
   plugins: [],
 }
+}
+
 export default config
