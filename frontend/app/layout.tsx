1606be9fc3d4903e64fb55d2e6b91d1d109283cb
Will Brammer <williamedwardbrammer@gmail.com>
2024-03-20 22:48:13 +0900

    feat: change the description in the metadata to include location for improved seo

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,21 +1,22 @@
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
-  title: 'Javco',
-  description: 'The best place to find trailers at an affordable price',
-}
+  title: "Javco",
+  description:
+    "The best place to find trailers at an affordable price. If you are looking for a semi-trailer in the Wisconsin area, this is the place to come. We have a variety of trailers from dry vans, reefers, flatbeds, tankers",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-      {children}
-      <Analytics />
+        {children}
+        <Analytics />
       </body>
     </html>
-  )
+  );
 }
