754452ec0057e4769c17192fbc1d8509d522d5c1
Will Brammer <williamedwardbrammer@gmail.com>
2024-05-08 15:28:48 +0900

    feat: implement dashboard

--- frontend/app/admin/dashboard/page.tsx
+++ frontend/app/admin/dashboard/page.tsx
@@ -7,6 +7,9 @@ import {
   adminAlreadyLoggedInCheck,
 } from "@/globalFunctions/apiCalls/apiCalls";
 import { useRouter } from "next/navigation";
+import NavBar from "@/components/navbarComponents/NavBar";
+import "../../globals.css";
+import scrollToSection from "@/globalFunctions/scrollToSections";
 
 const Dashboard = () => {
   const [authSuccessful, setAuthSuccessful] = useState<boolean>(false);
@@ -18,9 +21,20 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div>
-      {!authSuccessful ? <MoonLoader /> : <div>Welcome to the dashboard</div>}
-    </div>
+    <section className="bg-gray-100 p-8 relative">
+      <NavBar scrollToSection={scrollToSection} />
+      <div>
+        {!authSuccessful ? (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50" // Full-screen overlay with opaque background
+          >
+            <MoonLoader size={50} color="#2164a6" />
+          </div>
+        ) : (
+          <select></select>
+        )}
+      </div>
+    </section>
   );
 };
 
