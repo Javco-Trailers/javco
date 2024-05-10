2be5ce391d09288667a845d6e173761d608ccb43
Will Brammer <williamedwardbrammer@gmail.com>
2024-05-10 15:28:27 +0900

    feat: implement getting text from server

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -12,7 +12,23 @@ import { fetchText } from "@/globalFunctions/apiCalls/apiCalls";
 
 const Home: React.FC = async () => {
   const text = await fetchText();
-  console.log(text);
+  // Extract the first item from the text array
+  const textData = text.text[0].text;
+
+  // Parse the text to get the actual JSON object
+  const parsedText = JSON.parse(textData);
+
+  // Now you can access the Home_Page key
+  const homePage = parsedText.text.Home_Page;
+
+  // Access the keys within Home_Page
+  const mainHeader = homePage.Main_Header;
+  const secondHeader = homePage.Second_Header;
+  const typesOfTrailers = homePage.Types_Of_Trailers;
+  const infoAboutJavco = homePage.Info_About_Javco;
+  const linkText = homePage.Link_Text;
+  const linkToInventory = homePage.Link_To_Inventory;
+
   return (
     <>
       <div className="overflow-x-hidden">
@@ -20,9 +36,7 @@ const Home: React.FC = async () => {
           <NavBar />
         </div>
         <div className="bg-jblue text-white text-center mb-2 p-2">
-          <h1 className="font-bold text-5xl p-2">
-            Welcome to Javco Trailer Sales Inc.
-          </h1>
+          <h1 className="font-bold text-5xl p-2">{mainHeader}</h1>
         </div>
         <div className="flex items-center justify-center mb-4">
           <Image
@@ -35,34 +49,30 @@ const Home: React.FC = async () => {
         </div>
         <div className="text-center">
           <h1 className="text-3xl md:text-4xl font-bold mb-4 text-jblue">
-            Your Premier Destination for Affordable and Exceptional
-            Semi-Trailers!
+            {secondHeader}
           </h1>
           <div className="bg-jblue text-white text-center">
             <div className="flex flex-col justify-center items-center">
               <div className="font-semibold flex flex-row justify-between p-2 w-3/5 md:w-1/5">
-                <p>-Dry Vans</p>
-                <p>-Reefers</p>
+                <p>{typesOfTrailers.Type_One}</p>
+                <p>{typesOfTrailers.Type_Two}</p>
               </div>
-              <div className="font-semibold flex flex-row w-2/5 justify-between p-2 w-3/5 md:w-1/5">
-                <p>-Flatbeds</p>
-                <p>-Tankers</p>
+              <div className="font-semibold flex flex-row w-2/5 justify-between p-2 md:w-1/5">
+                <p>{typesOfTrailers.Type_Three}</p>
+                <p>{typesOfTrailers.Type_Four}</p>
               </div>
             </div>
-            <p className="p-2 text-xl font-semibold">
-              Need a trailer? Javco has you covered! We offer a wide selection
-              of Semi-trailers for storage, cartage & OTR use.
-            </p>
+            <p className="p-2 text-xl font-semibold">{infoAboutJavco}</p>
             <h2 className="font-bold text-2xl mb-4 p-2">
               <a href="/rentals">
-                <u>Rent</u>
+                <u>{linkText.Link_To_Rental_Text}</u>
               </a>{" "}
-              or{" "}
+              {linkText.Filler}
               <a href="/sales">
-                <u>Buy</u>
+                <u>{linkText.Link_To_Sales_Text}</u>
               </a>{" "}
               <Link href="mailto:info@javco.co?subject=Javco Inquiry">
-                <u>NOW!</u>
+                <u>{linkText.Link_To_Email}</u>
               </Link>
             </h2>
             <h2>
@@ -70,7 +80,7 @@ const Home: React.FC = async () => {
                 className="text-white font-bold text-2xl underline"
                 href={"/inventory"}
               >
-                Click here to see view our current inventory!
+                {linkToInventory}
               </Link>
             </h2>
           </div>
