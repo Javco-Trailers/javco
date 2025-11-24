ca428795933e15914eafd2a53f4f35d4e6fde865
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-11-25 08:47:23 +0900

    chore: fix spacing of text

--- frontend/components/servicePageComponents/SellTrailers.tsx
+++ frontend/components/servicePageComponents/SellTrailers.tsx
@@ -13,12 +13,12 @@ const SellTrailers= () => {
               </h2>
               <ul>
                 <li className="ml-6 text-gray-600">
-                  - Thinking of selling your trailer? Javco is always looking for quality inventory at a fair price. Whether you want to trade-in or just downsize your fleet, we'll make the process easy and pain-free. Tell us what you have
+                  {"- Thinking of selling your trailer? Javco is always looking for quality inventory at a fair price. Whether you want to trade-in or just downsize your fleet, we'll make the process easy and pain-free. Tell us what you have "}
                   <Link
                     className="underline font-semibold text-jblue"
                     href="mailto:info@javco.co?subject=Javco Inquiry For Selling Trailer"
                   >
-                    {" by sending us an email."}
+                    {"by sending us an email."}
                   </Link>{" "}
                   .
                 </li>
