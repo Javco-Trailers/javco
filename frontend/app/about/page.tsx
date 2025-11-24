e561d3463e82a4faa0c92f3940b889f39ac6152f
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-11-25 08:37:31 +0900

    fix: error in props for AboutUsSection

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,19 +1,13 @@
 "use server";
 
 import "../globals.css";
-import { fetchText } from "@/globalFunctions/apiCalls/apiCalls";
 import NavBar from "@/components/navbarComponents/NavBar";
 import ContactUs from "@/components/contactUs/contactUs";
 import Copyright from "@/components/copyright/copyRight";
 import AboutUsSection from "@/components/aboutUs/AboutUsSection";
 
 export default async function Home() {
-  const text = await fetchText(null);
-  // Extract the first item from the text array
-  const textData = text.text[0].text;
 
-  // Parse the text to get the actual JSON object
-  const parsedText = JSON.parse(textData);
   return (
     <>
       <div className="bg-white text-jblue mb-2 shadow-lg shadow-indigo-500/40 rounded-b px-8">
@@ -21,7 +15,7 @@ export default async function Home() {
       </div>
 
       <div>
-        <AboutUsSection parsedText={parsedText} />
+        <AboutUsSection />
       </div>
 
       <div id="contact">
