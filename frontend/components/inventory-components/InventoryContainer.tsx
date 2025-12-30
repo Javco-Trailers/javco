a52841b7c5c78340d30f3e2f2d916d07a7f1d869
NabbeunNabi <williamedwardbrammer@gmail.com>
2025-12-30 09:47:49 +0900

    chore: upgrade dependencies and javco iframe id

--- frontend/components/inventory-components/InventoryContainer.tsx
+++ frontend/components/inventory-components/InventoryContainer.tsx
@@ -1,14 +1,10 @@
+"use client"
 import "../../app/globals.css";
-
-import { InventoryItem } from "@/app/types/types";
-
-import InventoryCard from "./InventoryCard";
-
 
 const InventoryContainer: React.FC = () => {
   return (
     <section className="bg-gray-100 p-8 relative">
-      <iframe src="https://cherrytrader.com/embed/seller/javco-trailer-sales-inc-f24f6d" width="100%" height="1080" style={{border:0}} />
+      <iframe src="https://cherrytrader.com/embed/seller/javco-trailer-sales-inc-ef07a243" width="100%" height="1080" style={{border:0}} />
     </section>
   );
 };
